3f4b8e208d475c147ec74d2c2c9285385132b600
LAZKORIR <lazaruskorir95@gmail.com>
2023-03-09 23:53:26 +0300

    first commit

---
+++ ms-lending-platform/src/main/java/com/ims/mslendingplatform/MsLendingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ims.mslendingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLendingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLendingPlatformApplication.class, args);
+	}
+
+}
---
+++ ms-lending-platform/src/test/java/com/ims/mslendingplatform/MsLendingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.mslendingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLendingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
